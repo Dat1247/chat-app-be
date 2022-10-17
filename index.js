54814983742020c65c5e582eae4d14ff8d6213c0
Ho Dat <hoquandat1247@gmail.com>
2022-10-17 13:11:35 +0700

    Change domain to connect socketIo in front-end

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(port || process.env.PORT, () => {
 
 const io = socket(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "https://hqd-chat-app.netlify.app",
 		credentials: true,
 	},
 });
